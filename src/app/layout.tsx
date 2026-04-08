e5bd2ed2e5132b9dfb204c369848c84a2b4f44ef
koki <137967060+kokiishikawa@users.noreply.github.com>
2026-04-08 14:57:14 +0900

    feat: Vercel Analytics を導入

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,40 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import './globals.css';
+import { Analytics } from '@vercel/analytics/next';
 
 export const metadata: Metadata = {
-  title: "koki.ishikawa | Portfolio",
-  description: "koki.ishikawaのポートフォリオサイト",
-  
-  metadataBase: new URL("https://koki-ishikawa-portfolio.vercel.app"),
-  
-  openGraph: {
-    title: "koki.ishikawa | Portfolio",
-    description: "koki.ishikawaのポートフォリオサイト",
-    url: "https://koki-ishikawa-portfolio.vercel.app",
-    siteName: "koki.ishikawa",
-    locale: "ja_JP",
-    type: "website"
-  },
+	title: 'koki.ishikawa | Portfolio',
+	description: 'koki.ishikawaのポートフォリオサイト',
 
-  twitter: {
-    card: "summary_large_image",
-    title: "koki.ishikawa | Portfolio",
-    description: "koki.ishikawaのポートフォリオサイト",
-  },
+	metadataBase: new URL('https://koki-ishikawa-portfolio.vercel.app'),
+
+	openGraph: {
+		title: 'koki.ishikawa | Portfolio',
+		description: 'koki.ishikawaのポートフォリオサイト',
+		url: 'https://koki-ishikawa-portfolio.vercel.app',
+		siteName: 'koki.ishikawa',
+		locale: 'ja_JP',
+		type: 'website',
+	},
+
+	twitter: {
+		card: 'summary_large_image',
+		title: 'koki.ishikawa | Portfolio',
+		description: 'koki.ishikawaのポートフォリオサイト',
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="ja">
-      <body>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="ja">
+			<body>
+				{children}
+				<Analytics />
+			</body>
+		</html>
+	);
 }
